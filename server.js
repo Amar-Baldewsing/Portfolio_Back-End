0cb3c6ec8bcbb78c1fb50a3edf461639f233c1c6
Amar Baldewsing <amardevelopment05@gmail.com>
2024-12-18 17:04:01 +0100

    PF_B4: fix wrong DNS server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "www.amarbaldewsing.nl");
+    res.header("Access-Control-Allow-Origin", "https://www.amarbaldewsing.nl/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
